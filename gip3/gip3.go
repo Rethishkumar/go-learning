34fc4f63e40b93eefd3a377d58d3fec98d54cd92
Rethish <rnair@akamai.com>
2020-06-21 14:23:28 +0530

    till chapter 3 locks

--- gip3/gip3.go
+++ gip3/gip3.go
@@ -42,52 +42,125 @@ package main
 // 	runtime.Gosched()
 // }
 
+// /*
+// Listing 3.4 Compressing files in parallel with a wait group
+// */
+// import (
+// 	"compress/gzip"
+// 	"fmt"
+// 	"io"
+// 	"os"
+// 	"sync"
+// )
+
+// func main() {
+// 	var wg sync.WaitGroup
+
+// 	var i int = -1
+// 	var file string
+// 	for i, file = range os.Args[1:] {
+// 		wg.Add(1)
+// 		go func(filename string) {
+// 			compress(filename)
+// 			wg.Done()
+// 		}(file)
+// 	}
+
+// 	wg.Wait()
+
+// 	fmt.Printf("Compressed %d files\n", i+1)
+// }
+
+// func compress(filename string) error {
+// 	in, err := os.Open(filename)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	defer in.Close()
+
+// 	out, err := os.Create(filename + ".gz")
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	defer out.Close()
+
+// 	gzout := gzip.NewWriter(out)
+// 	_, err = io.Copy(gzout, in)
+// 	gzout.Close()
+// 	return err
+// }
+
 /*
-Listing 3.4 Compressing files in parallel with a wait group
+	Listing 3.6 Word counter with locks
 */
+
 import (
-	"compress/gzip"
+	"bufio"
 	"fmt"
-	"io"
 	"os"
+	"strings"
 	"sync"
 )
 
 func main() {
 	var wg sync.WaitGroup
 
-	var i int = -1
-	var file string
-	for i, file = range os.Args[1:] {
+	w := newWords()
+	for _, f := range os.Args[1:] {
 		wg.Add(1)
 		go func(filename string) {
-			compress(filename)
+			if err := tallyWords(filename, w); err != nil {
+				fmt.Println(err.Error())
+			}
 			wg.Done()
-		}(file)
+		}(f)
 	}
 
 	wg.Wait()
 
-	fmt.Printf("Compressed %d files\n", i+1)
+	fmt.Println("Words that appear more than once")
+	for word, count := range w.found {
+		if count > 1 {
+			fmt.Printf("%s occured %d times \n", word, count)
+		}
+	}
 }
 
-func compress(filename string) error {
-	in, err := os.Open(filename)
+type words struct {
+	sync.Mutex
+	found map[string]int
+}
+
+func newWords() *words {
+	return &words{found: map[string]int{}}
+}
+
+func (w *words) add(word string, n int) {
+	w.Lock()
+	defer w.Unlock()
+	count, ok := w.found[word]
+	if !ok {
+		w.found[word] = n
+		return
+	}
+	w.found[word] = count + n
+}
+
+func tallyWords(filename string, dict *words) error {
+	file, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
 
-	defer in.Close()
+	defer file.Close()
 
-	out, err := os.Create(filename + ".gz")
-	if err != nil {
-		return err
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		word := strings.ToLower(scanner.Text())
+		dict.add(word, 1)
 	}
-
-	defer out.Close()
-
-	gzout := gzip.NewWriter(out)
-	_, err = io.Copy(gzout, in)
-	gzout.Close()
-	return err
+	return scanner.Err()
 }
